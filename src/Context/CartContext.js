42577e8ba696e65883fb1c06a8dd24517e01f808
LauraDoria <lau.doria94@hotmail.com>
2022-05-17 20:24:35 -0300

    Proyecto Final

--- src/Context/CartContext.js
+++ src/Context/CartContext.js
@@ -2,19 +2,14 @@ import { useState, createContext } from "react";
 
 const CartContext = createContext()
 
-/*Para envolver toda la app con un Provider que no sea creado dentro de App, paso todos los
-componentes hijos {childern} como props al componente CartContextProvder*/
 export const CartContextProvider = ({children}) => {
-    
-    //Crear nuevo estado.
+
     const [cart, setCart] = useState([])
 
-    //Setear estado al agregar productos
     const addProductToCart = (productToAdd) => {
         setCart([...cart, productToAdd])
     }
-    
-    //Obtener cantidad de productos
+
     const getProductQuantity = () => {
         let totalCount = 0
         cart.forEach(product => {
@@ -22,18 +17,15 @@ export const CartContextProvider = ({children}) => {
         })
         return totalCount
     }
-        
-    //Indicar si el producto ya fue agregado al carrito
+
     const isInCart = (id) => {
         return cart.some(product => product.id === id )
     }
-    
-    //Vaciar carrito
+
     const clearCart = () => {
         setCart([])
     }
 
-    //Total carrito
     const cartTotal = () => {
         let total = 0
         cart.forEach(product => {
@@ -42,16 +34,6 @@ export const CartContextProvider = ({children}) => {
         return total
     }
 
-    //Cantidad de productos
-    // const cartItemQuantity = () => {
-    //     let productCount = 0
-    //     cart.forEach(product => {
-    //         productCount += product.quantity
-    //     })
-    //     return productCount
-    // }
-    
-    //Eliminar del carrito
     const removeItemFromCart = (id) => {
         const products = cart.filter(product => product.id !== id)
         setCart(products)
